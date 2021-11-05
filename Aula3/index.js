32e2ff50dbb734df008ea964880696f46f982c69
Adsinho <Adson.mozart32@outlook.com>
2021-11-05 09:45:08 -0300

    PrimeiroUP

---
+++ Aula3/index.js
@@ -0,0 +1,5 @@
+console.log('Adson "Mozart"'); //String = texto
+console.log("Adson 'Mozart'");
+console.log(`'Adson' "Mozart"`);
+
+console.log(124356, 654321, 123.007, "Adson", "Mozart", 'Santos', `Paixão`);
